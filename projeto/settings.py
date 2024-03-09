34f594716b16fc6dbce62f40fd35ec03b85f9a39
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-03-08 21:16:01 -0300

    criação do app 'tarefas'

--- projeto/settings.py
+++ projeto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Apps
     'usuarios',
+    'tarefas',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
---
+++ tarefas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from usuarios.models import Usuario
+
+# Create your models here.
+class Tarefa(models.Model):
+    id_tarefa = models.AutoField(primary_key=True, editable=False)
+    titulo = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    data = models.DateField()
+    hora = models.TimeField()
+    usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Instância: {self.id_tarefa}, Entidade: {self.__class__.__name__}'
